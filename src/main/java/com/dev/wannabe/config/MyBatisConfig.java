b871b6d526a4e28e193b51fe7f6c026f219dcc8a
cjw <wlsdnrdl95@gmail.com>
2025-03-07 16:33:43 +0900

    1. DB연결 설정 xml 설정 -> java config 설정으로 변환

---
+++ src/main/java/com/dev/wannabe/config/MyBatisConfig.java
@@ -0,0 +1,72 @@
+package com.dev.wannabe.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.AllArgsConstructor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+@Configuration
+@MapperScan(basePackages = "com.dev.wannabe")
+public class MyBatisConfig {
+
+    @Value("${wannabe.datasource.driver-class}")
+    private String driverClass;
+
+    @Value("${wannabe.datasource.url}")
+    private String dbUrl;
+
+    @Value("${wannabe.datasource.username}")
+    private String dbUsername;
+
+    @Value("${wannabe.datasource.password}")
+    private String dbPassword;
+
+    @Value("${wannabe.datasource.wallet-path}")
+    private String walletPath;
+
+    @Bean
+    public DataSource dataSource() throws IOException {
+        HikariConfig config = new HikariConfig();
+
+        config.setDriverClassName(driverClass);
+        config.setJdbcUrl(dbUrl+walletPath);
+        config.setUsername(dbUsername);
+        config.setPassword(dbPassword);
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(5);
+        config.setValidationTimeout(5000);
+        config.setIdleTimeout(200000);
+        config.setMaxLifetime(240000);
+        config.setAutoCommit(false);
+
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setConfigLocation(new ClassPathResource("/META-INF/mybatis/sql-mapper-config.xml"));
+        factoryBean.setMapperLocations(new PathMatchingResourcePatternResolver()
+                .getResources("classpath:/META-INF/mybatis/sql/**/*.xml"));
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
