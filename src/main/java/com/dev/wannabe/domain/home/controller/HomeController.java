0e11bdac5e953d4d99a84847ea9d2f8d8d957aad
cjw <wlsdnrdl95@gmail.com>
2025-03-11 20:06:47 +0900

    1. 샘플코드 작성으로 인한 TestMapper 삭제 2. 패키지 구조 변경.

--- src/main/java/com/dev/wannabe/domain/home/controller/HomeController.java
+++ src/main/java/com/dev/wannabe/domain/home/controller/HomeController.java
@@ -1,27 +1,17 @@
-package com.dev.wannabe.home.controller;
+package com.dev.wannabe.domain.home.controller;
 
-import com.dev.wannabe.home.mapper.TestMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class HomeController {
 
-    private final TestMapper testMapper;
-
     @RequestMapping("/home")
     public String home(){
-        Map<String, Object> stringObjectMap = testMapper.testQuery();
-
-        for (String s : stringObjectMap.keySet()) {
-            log.info("s => {}", s);
-        }
         
         return "home";
     }
