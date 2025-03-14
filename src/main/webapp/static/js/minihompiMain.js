d0c29c6a9ca05d0c88dcb930147c86d34f9f2568
mobcoon <zccx33@naver.com>
2025-03-15 01:11:53 +0900

    [FIX] 미니홈피 CSS 수정

--- src/main/webapp/static/js/minihompiMain.js
+++ src/main/webapp/static/js/minihompiMain.js
@@ -1,3 +1,4 @@
+/*미니홈피 팝업창 설정*/
 function openPop() {
     var popupW = 1280;
     var popupH = 720;
@@ -9,8 +10,8 @@ function openPop() {
         'width=' + popupW + ',height=' + popupH + ',left=' + left + ',top=' + top);
 }
 
-window.openPop = openPop;
 
+/*미니홈피 우측메뉴 페이지 이동 겸 버튼 색 바뀌는 기능*/
 function menuMovePage(e, url) {
     console.log(e.currentTarget);
     $.ajax({
@@ -50,15 +51,38 @@ function menuMoveHomePage(e, url) {
             alert("페이지 로딩에 실패했습니다.\n오류내용: " + error);
         }
     });
+}
 
-    var btns = document.querySelectorAll("li");
-    btns.forEach(function (btn, i) {
-        if (e.currentTarget == btn) {
-            btn.classList.add("on");
+
+/* 메인 타이틀 수정 버튼 스왑 기능 */
+$(document).ready(function () {
+    $(".rightMenu").on("click", "#titleBtn", function () {
+        const $btn = $(this);
+
+        if ($btn.val() === '수정') {
+            const $title = $("#mainTitle");
+            const currentTitle = $title.text();
+
+            const $input = $("<input>", {
+                type: "text",
+                id: "mainTitleInput",
+                value: currentTitle
+            });
+            $title.replaceWith($input);
+            $btn.val("저장");
+            $btn.addClass("save-mode");
+            $input.focus();
         } else {
-            btn.classList.remove("on");
+            const newTitle = $("#mainTitleInput").val(); // 수정한 값을 가져옴
+            const $span = $("<span>", {
+                id: "mainTitle",
+                text: newTitle
+            });
+            $("#mainTitleInput").replaceWith($span);
+            $btn.val("수정");
+            $btn.removeClass("save-mode");
         }
     });
-    console.log(e.currentTarget);
-}
+});
+
 
