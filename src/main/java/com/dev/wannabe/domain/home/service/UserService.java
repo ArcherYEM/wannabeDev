15657e70771c625715682615f9883adde1cc576f
SeokjinDev <tjrwls1108011@gmail.com>
2025-03-16 02:29:15 +0900

    [FEAT] Add SQL Session

--- src/main/java/com/dev/wannabe/domain/home/service/UserService.java
+++ src/main/java/com/dev/wannabe/domain/home/service/UserService.java
@@ -7,6 +7,8 @@ import com.dev.wannabe.domain.home.model.vo.UserDetail;
 import com.dev.wannabe.domain.home.model.vo.UserRole;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,7 @@ public class UserService {
 
     private final UserMapper userMapper;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final SqlSessionFactory sqlSessionFactory;
 
     /*
      * 회원 가입 기능
@@ -28,53 +31,62 @@ public class UserService {
      * 저장 후 return 200 ok
      */
     public HttpStatus signUpUser(SignupUserDTO signupUser) {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try{
+            if (isExistByLoginId(signupUser.getLoginId())) {
+                return HttpStatus.BAD_REQUEST;
+            }
+            if (isExistByEmail(signupUser.getEmail())) {
+                return HttpStatus.BAD_REQUEST;
+            }
+            if (isExistByPhoneNo(signupUser.getPhoneNo())) {
+                return HttpStatus.BAD_REQUEST;
+            }
 
-        if (isExistByLoginId(signupUser.getLoginId())) {
+            String userId = UUID.randomUUID().toString();
+
+            UserBasic newUserBasic = UserBasic.builder()
+                    .userId(userId)
+                    .loginId(signupUser.getLoginId())
+                    .email(signupUser.getEmail())
+                    .phoneNo(signupUser.getPhoneNo())
+                    .password(passwordEncoder.encode(signupUser.getPassword()))
+                    .name(signupUser.getName())
+                    .genderCode(signupUser.getGenderCode())
+                    .birthDate(signupUser.getBirthDate())
+                    .userStatus("N")
+                    .insertUserId(userId)
+                    .build();
+
+            UserDetail newUserDetail = UserDetail.builder()
+                    .userId(userId)
+                    .friendMessageAvailYN(signupUser.getFriendMessageAvailYN())
+                    .hompiUseYN(signupUser.getHompiUseYN())
+                    .confirmYN1(signupUser.getConfirmYN1())
+                    .confirmYN2(signupUser.getConfirmYN2())
+                    .confirmYN3(signupUser.getConfirmYN3())
+                    .insertUserId(userId)
+                    .build();
+
+            UserRole newUserRole = UserRole.builder()
+                    .roleCd("03")
+                    .userId(userId)
+                    .insertUserId(userId)
+                    .build();
+
+            userMapper.saveUserBasic(newUserBasic);
+            userMapper.saveUserDetail(newUserDetail);
+            userMapper.saveUserRole(newUserRole);
+
+            sqlSession.commit();
+            sqlSession.close();
+            return HttpStatus.OK;
+        } catch (Exception e) {
+            sqlSession.rollback();
+            sqlSession.close();
             return HttpStatus.BAD_REQUEST;
         }
-        if (isExistByEmail(signupUser.getEmail())) {
-            return HttpStatus.BAD_REQUEST;
-        }
-        if (isExistByPhoneNo(signupUser.getPhoneNo())) {
-            return HttpStatus.BAD_REQUEST;
-        }
 
-        String userId = UUID.randomUUID().toString();
-
-        UserBasic newUserBasic = UserBasic.builder()
-                .userId(userId)
-                .loginId(signupUser.getLoginId())
-                .email(signupUser.getEmail())
-                .phoneNo(signupUser.getPhoneNo())
-                .password(passwordEncoder.encode(signupUser.getPassword()))
-                .name(signupUser.getName())
-                .genderCode(signupUser.getGenderCode())
-                .birthDate(signupUser.getBirthDate())
-                .userStatus("N")
-                .insertUserId(userId)
-                .build();
-
-        UserDetail newUserDetail = UserDetail.builder()
-                .userId(userId)
-                .friendMessageAvailYN(signupUser.getFriendMessageAvailYN())
-                .hompiUseYN(signupUser.getHompiUseYN())
-                .confirmYN1(signupUser.getConfirmYN1())
-                .confirmYN2(signupUser.getConfirmYN2())
-                .confirmYN3(signupUser.getConfirmYN3())
-                .insertUserId(userId)
-                .build();
-
-        UserRole newUserRole = UserRole.builder()
-                .roleCd("03")
-                .userId(userId)
-                .insertUserId(userId)
-                .build();
-
-        userMapper.saveUserBasic(newUserBasic);
-        userMapper.saveUserDetail(newUserDetail);
-        userMapper.saveUserRole(newUserRole);
-
-        return HttpStatus.OK;
     }
 
     /*
