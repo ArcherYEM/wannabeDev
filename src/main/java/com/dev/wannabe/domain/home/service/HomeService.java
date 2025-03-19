ef35043ac068ecf3f763e7538c284565b46ce1c2
Dev.Yem <134784917+ArcherYEM@users.noreply.github.com>
2025-03-20 00:20:06 +0900

    [FEAT] 클라이언트 IP 가져오기

---
+++ src/main/java/com/dev/wannabe/domain/home/service/HomeService.java
@@ -0,0 +1,26 @@
+package com.dev.wannabe.domain.home.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+
+@Slf4j
+@Service
+public class HomeService {
+    // 클라이언트 아이피 가져오기
+    public String getIp(HttpServletRequest req){
+        String ip = req.getHeader("x-forwarded-for");
+
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = req.getRemoteAddr();
+        }
+
+        if ("0:0:0:0:0:0:0:1".equals(ip)){
+            ip = "127.0.0.1";
+        }
+
+        return ip;
+    }
+}
