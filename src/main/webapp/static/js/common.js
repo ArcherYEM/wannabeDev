158da32026469543ffc2088308eafa647b1e719f
대 왕 님 <em.yang94@gmail.com>
2025-03-21 01:01:11 +0900

    충돌 해결중

--- src/main/webapp/static/js/common.js
+++ src/main/webapp/static/js/common.js
@@ -2,3 +2,16 @@ $(function (){
 
 });
 
+/** 미니홈피 팝업창 설정 **/
+function openPop() {
+    const popupW = 1280;
+    const popupH = 720;
+    const left = Math.ceil((window.screen.width - popupW) / 2);
+    const top = Math.ceil((window.screen.height - popupH) / 2);
+
+    const hompiId = 0;
+
+    window.open(`/mini-hompi/main/${hompiId}`,
+        'mini-hompi',
+        'width=' + popupW + ',height=' + popupH + ',left=' + left + ',top=' + top);
+}
