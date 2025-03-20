6ff2a7f2c8cc6a29f04758c92742ffd09f0bda3c
cjw <wlsdnrdl95@gmail.com>
2025-03-20 16:34:54 +0900

    [Fix] servlet-context.xml로 관리되던 웹 관련 Config 두 곳에서 관리되는 사항 보완

--- src/main/java/com/dev/wannabe/global/config/WebConfig.java
+++ src/main/java/com/dev/wannabe/global/config/WebConfig.java
@@ -1,12 +1,36 @@
 package com.dev.wannabe.global.config;
 
+import com.dev.wannabe.global.aop.interceptor.LoggingInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LoggingInterceptor loggingInterceptor() {
+        return new LoggingInterceptor();
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/", "file:src/main/webapp/static/");
+                .addResourceLocations("/static/", "file:src/main/webapp/static/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/static/**");
     }
 }
