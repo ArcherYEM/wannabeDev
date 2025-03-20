b556aff4b0177cac8b6d39a26ff66aae61191ecc
ycs-202007012 <kdj1785k@naver.com>
2025-03-21 01:26:20 +0900

    [FEAT]임시 커밋

--- src/main/java/com/dev/wannabe/domain/minihompi/mapper/BgmMapper.java
+++ src/main/java/com/dev/wannabe/domain/minihompi/mapper/BgmMapper.java
@@ -1,13 +1,10 @@
 package com.dev.wannabe.domain.minihompi.mapper;
 
-import com.dev.wannabe.domain.minihompi.model.vo.Bgm;
+import com.dev.wannabe.domain.minihompi.model.dto.HompiBgmDTO;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
-import java.util.Optional;
 
 @Mapper
 public interface BgmMapper {
 
+    HompiBgmDTO findOwnerBgmByOwnerId(Long ownerId);
 }
--- src/main/java/com/dev/wannabe/domain/minihompi/model/dto/HompiBgmDTO.java
+++ src/main/java/com/dev/wannabe/domain/minihompi/model/dto/HompiBgmDTO.java
@@ -5,7 +5,10 @@ import lombok.Data;
 
 @Data
 @Builder
-public class UserBgmDTO {
+public class HompiBgmDTO {
 
+    private String bgmName;
+    private String artist;
+    private String bgmPath;
 
 }
