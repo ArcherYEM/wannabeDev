05b3e426b0dc72a6ef2721df23ccd3bed1f7a01d
insun4210@gmail.com <insun4210@gmail.com>
2025-03-22 01:43:43 +0900

    [fix]날씨API완료

--- src/main/java/com/dev/wannabe/domain/home/mapper/WeatherLogMapper.java
+++ src/main/java/com/dev/wannabe/domain/home/mapper/WeatherLogMapper.java
@@ -1,10 +1,11 @@
 package com.dev.wannabe.domain.home.mapper;
 
-import com.dev.wannabe.domain.home.model.vo.WeatherLog;
+import com.dev.wannabe.domain.home.model.dto.WeatherLogDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-
 public interface WeatherLogMapper {
-    void insertWeatherLog(WeatherLog weatherLog);
+    WeatherLogDTO getLatestWeatherLog();         // 최신 데이터 조회
+    void insertWeatherLog(WeatherLogDTO dto);    // INSERT
+    void updateWeatherLog(WeatherLogDTO dto);    // UPDATE
 }
