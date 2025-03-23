d5b4883cb7b3d96fbd375fca1568276b5334716c
SeokjinDev <tjrwls1108011@gmail.com>
2025-03-24 04:35:28 +0900

    [FIX] DDL 오타 수정

--- src/main/resources/META-INF/mybatis/script/WANNABE_DEV_DDL.sql
+++ src/main/resources/META-INF/mybatis/script/WANNABE_DEV_DDL.sql
@@ -707,7 +707,7 @@ CREATE TABLE HOMPI_FOLDER (
                               HOMPI_ID BIGINT COMMENT '홈피 ID',
                               FOLDER_ID BIGINT AUTO_INCREMENT UNIQUE COMMENT '폴더 ID',
                               FOLDER_NAME VARCHAR(500) COMMENT '폴더 이름',
-                              CONTENT_TYPE VARCHAR(2) COMMENT '콘텐츠 종류',
+                              CONTENTS_TYPE VARCHAR(2) COMMENT '콘텐츠 종류',
                               REMARKS VARCHAR(500) COMMENT '비고',
                               INSERT_USER_ID BIGINT COMMENT '등록자 ID',
                               INSERT_DT DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '등록일시',
