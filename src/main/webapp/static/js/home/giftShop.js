7cd3e29232210a44831a29b26b4cc6f551c886ac
김자반 <128600341+kimseaweed@users.noreply.github.com>
2025-03-29 00:33:00 +0900

    [fix]아이템목록 슬라이드

--- src/main/webapp/static/js/home/giftShop.js
+++ src/main/webapp/static/js/home/giftShop.js
@@ -5,6 +5,14 @@ const recentSwiper = new Swiper("#recent-item-swiper", {
     spaceBetween: 25,
     mousewheel: true,
 });
+//카트 아이템리스트 슬라이드
+const itemBuySwiper = new Swiper("#item-swiper", {
+    direction: "vertical",
+    slidesPerView: 2.5,
+    spaceBetween: 25,
+    mousewheel: true,
+});
+
 
 $(function () {
     // 선물함 메뉴 효과
@@ -19,6 +27,14 @@ $(function () {
     })
     $('#down-arrow').on("click", function () {
         recentSwiper.slideNext()
+    })
+
+    //장바구니 아이템 위아래
+    $('#item-up-arrow').on("click", function () {
+        itemBuySwiper.slidePrev();
+    })
+    $('#item-down-arrow').on("click", function () {
+        itemBuySwiper.slideNext()
     })
 });
 
