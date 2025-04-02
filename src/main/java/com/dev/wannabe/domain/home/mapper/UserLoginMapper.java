e3c28c22c18403e88a0d9c6674760e6ecbcd82cc
SeokjinDev <tjrwls1108011@gmail.com>
2025-04-03 03:25:17 +0900

    [FEAT] 유저 로그인 확인 기능

---
+++ src/main/java/com/dev/wannabe/domain/home/mapper/UserLoginMapper.java
@@ -0,0 +1,15 @@
+package com.dev.wannabe.domain.home.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserLoginMapper {
+
+    void saveUserLogin(Long userId);
+    void deleteUserLogin(Long userId);
+
+    List<Long> loggedInFriendsByUserId(Long userId);
+
+}
