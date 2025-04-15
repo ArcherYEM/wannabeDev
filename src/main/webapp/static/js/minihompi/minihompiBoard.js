5a213ea7617fb6850d50799d231699b4886c737f
승우(슝슝)_2 <lsw0919@gmail.com>
2025-04-15 22:07:04 +0900

    [FIX] 미니홈피-게시판 부분 글쓰기부분 임시저장

--- src/main/webapp/static/js/minihompi/minihompiBoard.js
+++ src/main/webapp/static/js/minihompi/minihompiBoard.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
             currentPage = 1;
             $(".miniHP-Wrap").prepend(`<span class="boardFolderName">`+ $("button.folderOn").text().trim() +`</span>`);
             boardList(folderId, currentPage, pageSize);
-        }, 50);
+        }, 100);
 
 
 
@@ -181,10 +181,13 @@ function boardList(folderId, currentPage, pageSize) {
 }
 
 function renderPagination(totalPages) {
-    let paginationHtml = `<div class="btnTopArea dFlex">`
-    paginationHtml += `<button class="chkDeleteBtn">선택삭제</button>`;
-    paginationHtml += `<button class="WriterBoard" onclick="fncWriteBoard()">글쓰기</button>`;
-    paginationHtml += `</div>`;
+    let paginationHtml =``;
+    if (hompiId === userId){
+        paginationHtml = `<div class="btnTopArea dFlex">`
+        paginationHtml += `<button class="chkDeleteBtn">선택삭제</button>`;
+        paginationHtml += `<button class="WriterBoard" onclick="fncWriteBoard()">글쓰기</button>`;
+        paginationHtml += `</div>`;
+    }
     paginationHtml += `<div class="buttonWrap"><button id="prevPage" ${currentPage === 1 ? 'disabled' : ''}>&lt;&lt;</button>`;
     console.log("totalPages:", totalPages, "currentPage:", currentPage);
     for (let i = 1; i <= totalPages; i++) {
@@ -298,16 +301,287 @@ function fncWriteBoard(){
     console.log("게시판 글쓰기 fnc");
     $.ajax({
         type:"GET",
-        url: "/mini-hompi/board/writeBoardFrom",
-        success: function (responseForm) {
-            $(".miniHP-Wrap").html(responseForm);
+        url: "/minihompi/board/writeBoardFrom",
+        success: function (response) {
+            $(".miniHP-Wrap").html(response);
+            
+            setTimeout(function () {
+                response.append(loadCKEditer());
+            }, 50);
+            
         },
         error: function () {
             alert("비정상적인 접근입니다.");
         }
+        
     });
-
 }
 function fncUpdateBoard(){
     console.log("게시판 글 수정 fnc");
+}
+
+function loadCKEditer() {
+    const {
+        ClassicEditor,
+        Autoformat,
+        AutoImage,
+        Autosave,
+        BlockQuote,
+        Bold,
+        CKBox,
+        CKBoxImageEdit,
+        CloudServices,
+        Emoji,
+        Essentials,
+        Heading,
+        ImageBlock,
+        ImageCaption,
+        ImageInline,
+        ImageInsert,
+        ImageInsertViaUrl,
+        ImageResize,
+        ImageStyle,
+        ImageTextAlternative,
+        ImageToolbar,
+        ImageUpload,
+        Indent,
+        IndentBlock,
+        Italic,
+        Link,
+        LinkImage,
+        List,
+        ListProperties,
+        MediaEmbed,
+        Mention,
+        Paragraph,
+        PasteFromOffice,
+        PictureEditing,
+        Table,
+        TableCaption,
+        TableCellProperties,
+        TableColumnResize,
+        TableProperties,
+        TableToolbar,
+        TextTransformation,
+        TodoList,
+        Underline
+    } = window.CKEDITOR;
+
+    const LICENSE_KEY =
+        'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NDU5NzExOTksImp0aSI6IjFmYTYzZGM0LThkZjUtNGFkYi1hZmE2LTEzODZjN2RhNmEyMCIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6ImMwNDEyY2M0In0.iYEzFowD3xor88KYsibatLkT-azxjGQNVIT8qXAOcOvKDxgxHTLA3huNoL46y7-EaktGFP2y0FYyl732mLE8Tw';
+
+    const CLOUD_SERVICES_TOKEN_URL =
+        'https://1dxhn4uvos7i.cke-cs.com/token/dev/3ce840bebcf15ef918395e01def1854276f31ea034b2046e5cab6376b7c2?limit=10';
+
+    const editorConfig = {
+        toolbar: {
+            items: [
+                'heading',
+                '|',
+                'bold',
+                'italic',
+                'underline',
+                '|',
+                'emoji',
+                'link',
+                'insertImage',
+                'ckbox',
+                'mediaEmbed',
+                'insertTable',
+                'blockQuote',
+                '|',
+                'bulletedList',
+                'numberedList',
+                'todoList',
+                'outdent',
+                'indent'
+            ],
+            shouldNotGroupWhenFull: false
+        },
+        plugins: [
+            Autoformat,
+            AutoImage,
+            Autosave,
+            BlockQuote,
+            Bold,
+            CKBox,
+            CKBoxImageEdit,
+            CloudServices,
+            Emoji,
+            Essentials,
+            Heading,
+            ImageBlock,
+            ImageCaption,
+            ImageInline,
+            ImageInsert,
+            ImageInsertViaUrl,
+            ImageResize,
+            ImageStyle,
+            ImageTextAlternative,
+            ImageToolbar,
+            ImageUpload,
+            Indent,
+            IndentBlock,
+            Italic,
+            Link,
+            LinkImage,
+            List,
+            ListProperties,
+            MediaEmbed,
+            Mention,
+            Paragraph,
+            PasteFromOffice,
+            PictureEditing,
+            Table,
+            TableCaption,
+            TableCellProperties,
+            TableColumnResize,
+            TableProperties,
+            TableToolbar,
+            TextTransformation,
+            TodoList,
+            Underline
+        ],
+        cloudServices: {
+            tokenUrl: CLOUD_SERVICES_TOKEN_URL
+        },
+        heading: {
+            options: [
+                {
+                    model: 'paragraph',
+                    title: 'Paragraph',
+                    class: 'ck-heading_paragraph'
+                },
+                {
+                    model: 'heading1',
+                    view: 'h1',
+                    title: 'Heading 1',
+                    class: 'ck-heading_heading1'
+                },
+                {
+                    model: 'heading2',
+                    view: 'h2',
+                    title: 'Heading 2',
+                    class: 'ck-heading_heading2'
+                },
+                {
+                    model: 'heading3',
+                    view: 'h3',
+                    title: 'Heading 3',
+                    class: 'ck-heading_heading3'
+                },
+                {
+                    model: 'heading4',
+                    view: 'h4',
+                    title: 'Heading 4',
+                    class: 'ck-heading_heading4'
+                },
+                {
+                    model: 'heading5',
+                    view: 'h5',
+                    title: 'Heading 5',
+                    class: 'ck-heading_heading5'
+                },
+                {
+                    model: 'heading6',
+                    view: 'h6',
+                    title: 'Heading 6',
+                    class: 'ck-heading_heading6'
+                }
+            ]
+        },
+        image: {
+            toolbar: [
+                'toggleImageCaption',
+                'imageTextAlternative',
+                '|',
+                'imageStyle:inline',
+                'imageStyle:wrapText',
+                'imageStyle:breakText',
+                '|',
+                'resizeImage',
+                '|',
+                'ckboxImageEdit'
+            ]
+        },
+        initialData:
+            '내용을 입력해 주세요 !.',
+        language: 'ko',
+        licenseKey: LICENSE_KEY,
+        link: {
+            addTargetToExternalLinks: true,
+            defaultProtocol: 'https://',
+            decorators: {
+                toggleDownloadable: {
+                    mode: 'manual',
+                    label: 'Downloadable',
+                    attributes: {
+                        download: 'file'
+                    }
+                }
+            }
+        },
+        list: {
+            properties: {
+                styles: true,
+                startIndex: true,
+                reversed: true
+            }
+        },
+        mention: {
+            feeds: [
+                {
+                    marker: '@',
+                    feed: [
+                        /* See: https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html */
+                    ]
+                }
+            ]
+        },
+        placeholder: 'Type or paste your content here!',
+        table: {
+            contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+        }
+    };
+
+    configUpdateAlert(editorConfig);
+
+    ClassicEditor.create(document.querySelector('#editor'), editorConfig);
+    
+    function configUpdateAlert(config) {
+        if (configUpdateAlert.configUpdateAlertShown) {
+            return;
+        }
+
+        const isModifiedByUser = (currentValue, forbiddenValue) => {
+            if (currentValue === forbiddenValue) {
+                return false;
+            }
+
+            if (currentValue === undefined) {
+                return false;
+            }
+
+            return true;
+        };
+
+        const valuesToUpdate = [];
+
+        configUpdateAlert.configUpdateAlertShown = true;
+
+        if (!isModifiedByUser(config.cloudServices?.tokenUrl, '<YOUR_CLOUD_SERVICES_TOKEN_URL>')) {
+            valuesToUpdate.push('CLOUD_SERVICES_TOKEN_URL');
+        }
+
+        if (valuesToUpdate.length) {
+            window.alert(
+                [
+                    'Please update the following values in your editor config',
+                    'to receive full access to Premium Features:',
+                    '',
+                    ...valuesToUpdate.map(value => ` - ${value}`)
+                ].join('\n')
+            );
+        }
+    }
 }
